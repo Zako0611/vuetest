673b822545db75b2b62c1c5c417f60b079e49cd0
Zako0611 <zengkun0611@qq.com>
2020-08-17 10:49:28 +0800

    构建项目 element UI

---
+++ src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <h1>This is an login page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.login{
+    height: 100vh;
+    background-color: aquamarine;
+
+}
+
+</style>
